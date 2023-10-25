e67c27a3233f1e5aeccb0d9a4609a725766ee92e
JOMINJI991123 <118803309+JOMINJI991123@users.noreply.github.com>
2023-10-25 13:35:46 +0900

    Initial commit

---
+++ app/src/main/java/com/example/capston_2/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.example.capston_2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState : Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_layout)
+
+        val binding = ActivityMainBinding.inflate(login_layout)
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/capston_2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.capston_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.community_addpost_layout)
+    }
+}
