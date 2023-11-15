fea8da43cb14a39724f198a003d0f1780cf8dae6
User <kzoqtmxhselwkdls@gmail.com>
2023-11-15 14:41:31 +0900

    하단바 구현

--- ch6_view/src/main/java/com/example/ch6_view/MainActivity.kt
+++ ch6_view/src/main/java/com/example/ch6_view/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.example.ch6_view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private val homeFragment = HomeFragment()
+    private val settingFragment = SettingFragment()
+    private val infoFragment = InfoFragment()
+    private val rankFragment = RankFragment()
+    private val communityFragment = CommunityFragment()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigationview)
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.home -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.containers, homeFragment).commit()
+                    true
+                }
+                R.id.setting -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.containers, settingFragment).commit()
+                    true
+                }
+                R.id.info -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.containers, infoFragment)
+                        .commit()
+                    true
+                }
+                R.id.community -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.containers, communityFragment)
+                        .commit()
+                    true
+                }
+                    R.id.rank -> {
+                        supportFragmentManager.beginTransaction().replace(R.id.containers, rankFragment).commit()
+                        true
+                }
+                else -> false
+            }
+        }
+
+        // 디폴트로 표시할 프래그먼트 설정
+        supportFragmentManager.beginTransaction().replace(R.id.containers, homeFragment).commit()
     }
 }
