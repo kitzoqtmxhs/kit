67d0ac9eece5ead295f68cd80cb3a79d94f657a1
udg2019 <https://github.com/udg2019/helloapp3.git>
2023-12-03 15:24:59 +0900

    212

--- ch6_view/src/main/java/com/example/ch6_view/MainActivity.kt
+++ ch6_view/src/main/java/com/example/ch6_view/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.ch6_view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Switch
 import android.widget.TextView
+import android.widget.ToggleButton
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SwitchCompat
 
+
 class MainActivity : AppCompatActivity(){
+
+    var imageView: ImageView? = null
+    var toggleButton: ToggleButton? = null
+    var aSwitch: Switch? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,13 +22,16 @@ class MainActivity : AppCompatActivity(){
         //객체 생성
         val statusText: TextView = findViewById(R.id.status_text)
         val switchView: SwitchCompat = findViewById(R.id.switchView)
+        val statusCircle: ImageView = findViewById(R.id.imageView4)
 
         //switch 체크 이벤트
         switchView.setOnCheckedChangeListener { p0, isChecked ->
             if (isChecked) {
                 statusText.text = "앱 잠금 실행 중"
+                statusCircle.setImageResource(R.drawable.blue_circle2)
             } else {
                 statusText.text = "앱 잠금이 실행중이지 않아요"
+                statusCircle.setImageResource(R.drawable.blue_circle)
             }
         }
     }//onCreate
